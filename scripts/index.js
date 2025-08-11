8bbf0ed5c2a27c933fa2f0f4b03d696600280c33
Louise Aldridge <louisealdridge88@gmail.com>
2025-08-11 13:36:42 +0100

    Adding comments and fixing bugs

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,14 @@ const resultSection = document.getElementById("result-section")
 const modal = document.getElementById("modal")
 const innerModal = document.getElementById("inner-modal")
 
-
+// An error message that can be reused for error handling
 function renderNoResultsMessage() {
     resultSection.innerHTML = `
         <p class="error-message">Sorry, there was an error. Please try again. </p>
   `
 }
 
-//fetch data from api and handle errors
+//Fetch data from api and handle errors
 const fetchCountryData = async () => {
     let requestUrl = `https://restcountries.com/v3.1/all?fields=name,capital,currencies`
 
@@ -29,17 +29,20 @@ const fetchCountryData = async () => {
     }
 }
 
-//search btn functionality
+//The search button functionality. 
+// Preventing default form behaviour, fetching api data, and reseting the search input to be empty.
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault()
     fetchCountryData()
     searchInput.value = ""
 })
 
-//render details about about each country for search using map
+//Rendering details about about each country for search results using map
 function renderCountryDetails(countryData) {
     resultSection.innerHTML = ''
 
+    //Checking if country has previously been added to passport. 
+    // And removing countries with undefined fifas, as thats the id I'm using to add them to passport.
     const exisitingPages = localStorage.getItem("passport-pages")
     const passportPages = !exisitingPages ? [] : JSON.parse(exisitingPages)
     const countryDetailsHtml = countryData.filter((country) => country.fifa !== undefined).map((country) => {
@@ -48,55 +51,54 @@ function renderCountryDetails(countryData) {
             return passportPage.country === country.name.common
         })
 
-        //language and currency was nested objects with unique keys, so grabbing data this way
+        const addToPassportBtn = alreadySaved ? `<a href="./journal.html">View in your journal</a>` : `<button class="add-btn" data-add-button data-add-id="${country.fifa}"> Add to my passport </button>`
+
+        //Language and currency was nested objects with unique keys, so grabbing data this way.
         const language = Object.values(country.languages)[0]
         const { symbol, name } = Object.values(country.currencies)[0]
 
-        const addToPassportBtn = alreadySaved ? `<p>Already in your journal</p>` : `<button class="add-btn" data-add-button data-add-id="${country.fifa}"> Add to my passport </button>`
-
-
+        //The inner html showing details about the country in search results.
         return `
                 <div class="result-item">
-
                     <div class="img-section">
-                        
                         <img src=${country.flags.png} alt=${country.flags.alt} />
                     </div>
-                        <div class="detail-section">
-                        <h2>${country.name.common}</h1>
-                            <h3> Quick Facts:</h2>
-                            <ul>
+                   <div class="detail-section">
+                      <h2>${country.name.common}</h1>
+                       <h3> Quick Facts:</h2>
+                       <ul>
                             <li>The capital is ${country.capital[0]}</li>
                             <li>${country.name.common} is in ${country.region}</li>
                             <li>In ${country.name.common} they speak ${language}</li>
                             <li>${country.name.common} have a population of ${country.population}</li>
                             <li>The currency is ${name} (${symbol})</li>
-                            </ul>
-                        </div>
-                        <div class="btn-section"> 
-                            ${addToPassportBtn}
-                        </div>
+                        </ul>
+                    </div>
+                    <div class="btn-section"> 
+                        ${addToPassportBtn}
+                    </div>
                 </div>
             `
     }).join('')
 
-
     resultSection.innerHTML = countryDetailsHtml
 
+    //Using data sets for the 'Add to my passport' button, so that the countries data can also be used for the passport page.  
     document.querySelectorAll("[data-add-button]").forEach(button => {
         button.addEventListener("click", () => {
             const countryId = button.dataset.addId
             const country = countryData.find((country) => {
                 return countryId === country.fifa
             })
-            renderJournalModal(country)
+            //passing the country data down to the renderPassport function
+            renderPassportModal(country)
         })
     })
-
 }
 
-
-function renderJournalModal(countryData) {
+//Using the country data render the country name. 
+// And creating a form for data that can be used to render a passport page.
+function renderPassportModal(countryData) {
 
     innerModal.innerHTML = `
     <button class="close-btn" id="close-btn"><i class="fa-solid fa-circle-xmark"></i> </button>
@@ -161,7 +163,7 @@ function renderJournalModal(countryData) {
                 <label for="sight4"><i class="fa-solid fa-binoculars"></i> </label>
 
             <input type="radio" id="sight5" name="sights-rating" value="5" />
-                <label for="food5"><i class="fa-solid fa-binoculars"></i> </label>
+                <label for="sight5"><i class="fa-solid fa-binoculars"></i> </label>
     </fieldset>
     
     <fieldset class="star-rating"> 
@@ -189,24 +191,22 @@ function renderJournalModal(countryData) {
 </div>
     </form>
     `
-
     modal.style.display = "flex"
 
     document.getElementById("close-btn").addEventListener("click", () => {
         modal.style.display = 'none'
     })
-    //create inner html for modal and form
 
-    //each rating button
-
-    //submit form and data saves to local storage for journal page
+    //Submitting the form and saving data to a passportData object. 
+    // Which is then passed down to saveToPassport, so the data can be saved to localStorage and used for the passport pages. 
 
     const formElement = document.getElementById("journal-form")
     formElement.addEventListener("submit", (e) => {
         e.preventDefault()
 
+        //Using input names from the form to get submitted data
         const formData = new FormData(formElement)
-        const journalData = {
+        const passportData = {
             country: countryData.name.common,
             countryFlag: countryData.flags.png,
             countryAlt: countryData.flags.alt,
@@ -216,19 +216,19 @@ function renderJournalModal(countryData) {
             enjoymentRating: formData.get("joy-rating"),
             memories: formData.get("fav-memories")
         }
-        saveToJournal(journalData)
-
-
-
+        saveToPassport(passportData)
+        resultSection.innerHTML = ''
     })
+
+    //Adding an event listener to the radio buttons to capture values for the highlightRatings function
     formElement.querySelectorAll("input[type=radio]").forEach((radio) => {
         radio.addEventListener("change", (e) => {
             highlightRatings(e.target)
-
         })
     })
 }
 
+//Adding a class to fill icons that are less than or equal to the max value that user has selected.
 function highlightRatings(radioInput) {
     const group = radioInput.name
     const maxValue = radioInput.value
@@ -243,12 +243,11 @@ function highlightRatings(radioInput) {
     })
 }
 
-function saveToJournal(journalData) {
-
+//saving Data to localStorage and not overriding the existing data. 
+function saveToPassport(passportData) {
     const exisitingPages = localStorage.getItem("passport-pages")
     const passportPages = !exisitingPages ? [] : JSON.parse(exisitingPages)
-
-    passportPages.push(journalData)
+    passportPages.push(passportData)
     const savedPages = JSON.stringify(passportPages)
     localStorage.setItem("passport-pages", savedPages)
 
--- scripts/journal.js
+++ scripts/journal.js
@@ -1,38 +1,35 @@
 const journalSection = document.getElementById("journal-section")
 
-function renderJournal() {
+//Using the data saved to localStorage to render the passport page entry
+function renderPassport() {
     const storedPages = JSON.parse(localStorage.getItem("passport-pages"))
-    if (storedPages.length === 0) {
-        console.log("Your journal is empty")
+    if (storedPages === null) {
+        journalSection.innerHTML = `<p class="error-message">Your passport is empty. <a href="./index.html">Get started by searching for a country. </a> </p>`
     } else {
-        console.log(storedPages)
-        const pagesinnerHtml = storedPages.map((page) =>
-            `
+        const pagesinnerHtml = storedPages.map((page) => `
     <div class="journal-item">
-          <div class="img-section">
+        <div class="img-section">
             <img src="${page.countryFlag}" alt="${page.countryAlt}" />
-          </div>
-        <div>  
-          <div class="detail-section">
-            <h2>${page.country} </h2>
-            <h3>Your favourite memories</h3>
-            <p>${page.memories}</p>
         </div>
-            <div class="rating-section">
-            <ul> 
-                <li class="blue-label">${page.affordabilityRating}/5 for Affordability <i class="fa-solid fa-coins"></i></li>
-                <li class="purple-label">${page.foodRating}/5 for Food <i class="fa-solid fa-utensils"></i> </li>
-                <li class="orange-label">${page.sightsRating}/5 for the Sights <i class="fa-solid fa-binoculars"></i> </li>
-                <li class="green-label">${page.enjoymentRating}/5 for Enjoyment <i class="fa-solid fa-face-smile"></i> </li>
-            </ul>
-         </div>
+        <div>  
+            <div class="detail-section">
+                <h2>${page.country} </h2>
+                <h3>Your favourite memories</h3>
+                <p>${page.memories}</p>
             </div>
-         </div>   
+            <div class="rating-section">
+                <ul> 
+                    <li class="blue-label">${page.affordabilityRating}/5 for Affordability <i class="fa-solid fa-coins"></i></li>
+                    <li class="purple-label">${page.foodRating}/5 for Food <i class="fa-solid fa-utensils"></i> </li>
+                    <li class="orange-label">${page.sightsRating}/5 for the Sights <i class="fa-solid fa-binoculars"></i> </li>
+                    <li class="green-label">${page.enjoymentRating}/5 for Enjoyment <i class="fa-solid fa-face-smile"></i> </li>
+                </ul>
+            </div>
+        </div>
+    </div>   
             `
         ).join('')
         journalSection.innerHTML = pagesinnerHtml
     }
-
 }
-
-renderJournal()
+renderPassport()
